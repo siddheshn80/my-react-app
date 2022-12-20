4977751be336eee88b4fc07253a757b3325daca3
Siddhesh <siddheshn80@gmail.com>
2022-12-20 15:18:31 +0530

    multiline-multitag

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-// JSX!
-// JSX => JAVA SCRIPT + XML
-// XML => Extended the HTML and Cratedad new Tag
-// App => <App />
 function App() {
-    return <h1>Hello</h1>;
-  }
-  
-  /*function normalFn() {
-    // ..
-    // ..
-    // ..
-    return 100;
-    return "abcd"
-    return true;
-    return [];
-    return {};
-  }*/
-  
-  export default App;
+  return (
+    <div>
+      <Person name="Rahul" />
+      <Person name="Rohit" />
+      <Person name="Kharghar" />
+      <Person name="Belapur" />
+      <Person name="Nerul" />
+      <Person name="Panvel" />
+      <Person name="Lonavala" />
+    </div>
+  );
+}
+
+// <Person />
+function Person({ name }) {
+  return (
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Person Name : {name} </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
+        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
+        libero! Tempore, consequuntur!
+      </p>
+    </div>
+  );
+}
+
+export default App;
