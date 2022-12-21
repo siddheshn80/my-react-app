8658ad50a453d2e43ac1064a2a49b2b9d6a93979
Siddhesh <siddheshn80@gmail.com>
2022-12-21 09:38:13 +0530

    basic interpolation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
+
+// USER DEFINED TAGS == COMPONENT
 function App() {
-  return (
+
+  let project ="My React App";
+  let username = "siddhesh";
+
+
+
+  return(
     <div>
-      <Person name="Rahul" />
-      <Person name="Rohit" />
-      <Person name="america
-    " />
-      <Person name="welshet" />
-      <Person name="Nerul" />
-      <Person name="Panvel" />
-      <Person name="Lonavala" />
+      <h1>(project)</h1>
+    <h1>Hello {username}</h1>
+    
     </div>
   );
-}
 
-// <Person />
-function Person({ name }) {
-  return (
-    <div>
-      <img src="https://picsum.photos/400" alt="" />
-      <h1>Person Name : {name} </h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
-        delectus placeat? Consequuntur iusto perferendis voluptatibus, ipsa sit
-        non. Quo cumque veniam nostrum voluptatem qui accusantium ipsa labore
-        libero! Tempore, consequuntur!
-      </p>
-    </div>
-  );
 }
-
 export default App;
