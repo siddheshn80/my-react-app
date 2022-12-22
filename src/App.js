f1e3e751ed2607199679dce389b13f0bfcacf2df
Siddhesh <siddheshn80@gmail.com>
2022-12-22 11:58:39 +0530

    BASIC STYLING

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
     <div>
       <h1 className="beautify">EXTERNAL_______STYLLING!!!!!!!!</h1>
 
-      {/** This is kind of internal. Note: Only one curly braces. */}
-      <h1 style={mystyle}>Hello World</h1>
-
-      {/* THIS IS INLINE STYLING */}
-      <h1 style={{ color: "white", backgroundColor: "red" }}>Hello</h1>
+     
     </div>
   );
 }
