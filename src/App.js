69b08142df5e41cfb23cbdc5e5d0e9dd013a7a5d
Siddhesh <siddheshn80@gmail.com>
2022-12-22 18:22:00 +0530

    static-messageapp-ma-fn

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import { useState } from "react";
 
 function App() {
-  let [message, setMessage] = useState("Abc");
+  let [message, setMessage] = useState("Hello");
+  let [list, setList] = useState([
+    { message: "Hi", messageTime: new Date() },
+    { message: "How r u", messageTime: new Date() },
+    { message: "Good", messageTime: new Date() },
+    { message: "fine bro", messageTime: new Date() },
+  ]);
 
-  let updateMessage = (e) => {
-    // e.target == document.querySelector("input")
+  // Member fn
+  let updateInputMessage = (e) => {
     message = e.target.value;
     setMessage(message);
   };
 
+  let addMessage = () => {
+    // let newMessage = { message: "Chill bro...!", messageTime: new Date() };
+    let newMessage = { message: message, messageTime: new Date() };
+    list = [newMessage, ...list];
+    setList(list);
+  };
+
   return (
     <div>
-      <h1>Working with Input</h1>
+      <h1 className="bg-primary text-white p-3">Messaging Demo</h1>
 
-      <input
-        type="text"
-        placeholder="Enter message"
-        value={message}
-        onChange={updateMessage}
-      />
+      <div className="d-flex">
+        <input
+          className="form-control"
+          value={message}
+          onChange={updateInputMessage}
+          type="text"
+          placeholder="Enter Message"
+        />
+        <input
+          className="btn btn-primary"
+          type="button"
+          value="Add Message"
+          onClick={addMessage}
+        />
+      </div>
 
-      <h1>{message}</h1>
+      {list.map((item, index) => (
+        <div key={index} className="d-flex my-1">
+          <div className="badge text-bg-primary">
+            {item.message}
+            <span className="ms-4">
+              {item.messageTime.getHours()}:{item.messageTime.getMinutes()}
+            </span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
